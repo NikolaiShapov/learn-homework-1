a2c47e244f5b7a17aeac5762d79eb868803d586d
unknown <nikolius@inbox.ru>
2022-09-05 18:02:59 +0300

    Homework 6 and 7 exception

--- 6_exception1.py
+++ 6_exception1.py
@@ -4,17 +4,22 @@
 
 Исключения: KeyboardInterrupt
 
-* Перепишите функцию hello_user() из задания while1, чтобы она 
-  перехватывала KeyboardInterrupt, писала пользователю "Пока!" 
+* Перепишите функцию hello_user() из задания while1, чтобы она
+  перехватывала KeyboardInterrupt, писала пользователю "Пока!"
   и завершала работу при помощи оператора break
-    
+
 """
 
 def hello_user():
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
+    try:
+        while True:
+            reply = input('Как дела?')
+            if reply == 'Хорошо':
+                break
+    except KeyboardInterrupt:
+        print('\nПока!')
+        # завершала работу при помощи оператора break - не очень понял это условие и без break все заканчивается же ?
+
+
 if __name__ == "__main__":
     hello_user()
